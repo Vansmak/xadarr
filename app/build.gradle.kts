98fb9a88a5ebcf3a2718622d363324b9a1ddc39a
vansmak <vansmak@vberg.net>
2026-06-03 17:59:00 -0700

    2.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -30,8 +30,8 @@ android {
         // Fire TV devices can be as low as Android 7.1 (API 25) or lower depending on model/OS.
         minSdk = 23
         targetSdk = 35
-        versionCode = 308
-        versionName = "1.1"
+        versionCode = 309
+        versionName = "2.0"
         buildConfigField("String", "GITHUB_OWNER", "\"Vansmak\"")
         buildConfigField("String", "GITHUB_REPO", "\"xadarr\"")
 
